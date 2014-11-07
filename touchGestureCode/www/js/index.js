285ef9c67e9798ec237866a3bcc682bc1d78aaa7
TFyles <fylt1_13@uni.worc.ac.uk>
2014-11-07 11:05:16 +0000

    vhhydrtxj6
    tdrhtdhr

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -1,6 +1,6 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
-$(document).on("pagecreate","#pageone",function(){
+$(document).on("pagecreate","#overlay01",function(){
   
   	$('#tapholdtext').on("taphold",function(){
     	$(this).hide();
@@ -20,3 +20,77 @@ $(document).on("pagecreate","#pageone",function(){
 
 
 });
+
+$(document).on("pagecreate","#overlay02",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
+
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
+}
+
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+}
+
+<script type="text/javascript">
+$(function() {
+ 
+//set up hash detection 
+$(window).bind( 'hashchange', function(e) {
+ var hash = '';
+ var hashlenght = 0;
+ hashwith = '';
+ if (location.hash == ''){
+ location.hash = '#pages=index';
+ document.title = 'Work Index';
+ }
+ 
+ hash = location.hash;
+ hashlenght = hash.length;
+ hashwith = '#'+hash.substring(7, hashlenght);
+ hash = location.hash.substring(7, hashlenght);
+ if(hashwith != "#"+'index'){
+ show_overlay(hash);
+ document.title = 'Work '+hash;
+ }
+ else{
+ close_overlay();
+  
+ }
+  
+ });
+ 
+ $(window).trigger( 'hashchange' );
+ 
+ $(".overlays").each(function(){
+ $(this).click(function(){
+ close_overlay();
+ });
+ });
+ 
+});
+ 
+function close_overlay(){
+ $(".overlays").each(function(){
+ if($(this).css('display') == 'block'){$(this).hide();}
+ });
+ location.hash = '#pages=index';
+}
+ 
+//function to show overlay
+function show_overlay(aux){
+ $("#"+aux).show();
+}
+ 
+//function to change hash
+function change_hash(aux){
+ location.hash = '#pages='+aux;
+ document.title = 'Work '+aux;
+}
+ 
+</script>
